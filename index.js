ddb6a687698e6661d2a7ef2dd292b5c0f2151784
codeStackr <rdzaman20000@gmail.com>
2023-07-19 12:00:33 +0600

    fix eslint error

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import { Collection } from "./modules/collection.js";
 import { navFeature } from "./modules/navbar.js";
 import { dateTime } from "./modules/luxon..js";
 import {
-  titleInput,
-  authorInput,
-  form,
-  booksList,
+    titleInput,
+    authorInput,
+    form,
+    booksList,
 } from "./modules/elements.js";
 
 // Initialize a new Collection instance
@@ -14,28 +14,28 @@ const collection = new Collection();
 
 // Event listener for form submission
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Get input values
-  const title = titleInput.value;
-  const author = authorInput.value;
+    // Get input values
+    const title = titleInput.value;
+    const author = authorInput.value;
 
-  // Add the new book
-  collection.addBook(title, author);
+    // Add the new book
+    collection.addBook(title, author);
 });
 
 // Event listener for page load - bind Collection to maintain context
 document.addEventListener(
-  "DOMContentLoaded",
-  collection.loadBooks.bind(collection)
+    "DOMContentLoaded",
+    collection.loadBooks.bind(collection)
 );
 
 // Event listener for book removal
 booksList.addEventListener("click", (event) => {
-  if (event.target.classList.contains("remove-btn")) {
-    const id = parseInt(event.target.getAttribute("data-id"), 10);
-    collection.removeBook(id);
-  }
+    if (event.target.classList.contains("remove-btn")) {
+        const id = parseInt(event.target.getAttribute("data-id"), 10);
+        collection.removeBook(id);
+    }
 });
 navFeature();
 dateTime();
