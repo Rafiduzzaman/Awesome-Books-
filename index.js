487b9a41820303d3486d15de441e6cf605dd24c4
codeStackr <rdzaman20000@gmail.com>
2023-07-19 11:48:58 +0600

    Added files in modules and fixed linters errors

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-import './modules/Collection.js';
+// Import necessary elements and modules
+import { Collection } from './modules/collection.js';
+import { navFeature } from './modules/navbar.js';
+import { dateTime } from './modules/luxon..js';
+import {  titleInput,  authorInput,  form,  booksList,} from './modules/elements.js';
+
+// Initialize a new Collection instance
+const collection = new Collection();
+
+// Event listener for form submission
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Get input values
+  const title = titleInput.value;
+  const author = authorInput.value;
+
+  // Add the new book
+  collection.addBook(title, author);
+});
+
+// Event listener for page load - bind Collection to maintain context
+document.addEventListener(
+  'DOMContentLoaded',  collection.loadBooks.bind(collection),
+);
+
+// Event listener for book removal
+booksList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove-btn')) {
+    const id = parseInt(event.target.getAttribute('data-id'), 10);
+    collection.removeBook(id);
+  }
+});
+navFeature();
+dateTime();
